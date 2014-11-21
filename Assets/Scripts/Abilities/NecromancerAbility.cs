44b08ad5174d1cda45c970066b94c5be4e53747a
hli <henrik.lindstrom@lundalogik.se>
2014-11-21 07:48:02 +0100

    Really dont feel like playing Humas right now... those necros are EVIL!

---
+++ Assets/Scripts/Abilities/NecromancerAbility.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NecromancerAbility : MonoBehaviour
+{
+    public GameObject skeletonPrefab;
+    public float cooldown;
+    public float range;
+    public float radius;
+    public string raiseDeadAnimation;
+    public float tryAgainDelay;
+    public GameObject effectPrefab;
+
+    private float cooldownTimer = 0f;
+    private float tryAgainTimer = 0f;
+    private Animation _animation;
+    private UnitController unitController;
+    private Attackable attackable;
+
+    public void Start()
+    {
+        unitController = GetComponent<UnitController>();
+        attackable = GetComponent<Attackable>();
+        _animation = GetComponentInChildren<Animation>();
+        _animation[raiseDeadAnimation].wrapMode = WrapMode.Once;
+    }
+
+    public void Update()
+    {
+        if (cooldownTimer > 0f)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+
+        if (tryAgainTimer > 0f)
+        {
+            tryAgainTimer -= Time.deltaTime;
+        }
+
+        if (cooldownTimer <= 0f && tryAgainTimer <= 0f)
+        {
+            var colliders = Physics.OverlapSphere(transform.position, range);
+
+            Transform target = null;
+            int i = 0;
+            while (i < colliders.Length)
+            {
+                if (colliders[i].tag == "Unit")
+                {
+                    var attackable = colliders[i].GetComponent<Attackable>();
+                    if (attackable.dead)
+                    {
+                        target = colliders[i].transform;
+                        break;
+                    }
+                }
+                i++;
+            }
+
+            if (target != null)
+                CastAt(target);
+
+            tryAgainTimer = tryAgainDelay;
+        }
+    }
+
+    private void CastAt(Transform target)
+    {
+        _animation.CrossFade(raiseDeadAnimation);
+        _animation.CrossFadeQueued(unitController.walkAnimation);
+        Instantiate(effectPrefab, target.position, Quaternion.identity);
+
+        var colliders = Physics.OverlapSphere(target.position, radius);
+        int i = 0;
+        while (i < colliders.Length)
+        {
+            if (colliders[i].tag == "Unit")
+            {
+                var attackable = colliders[i].GetComponent<Attackable>();
+                if (attackable.dead)
+                {
+                    SpawnSkeletonAt(colliders[i].transform.position);
+                    Destroy(colliders[i].gameObject);
+                }
+            }
+            i++;
+        }
+        cooldownTimer = cooldown;
+    }
+
+    private void SpawnSkeletonAt(Vector3 position)
+    {
+        var skeleton = Instantiate(skeletonPrefab, position, Quaternion.identity) as GameObject;
+        skeleton.SendMessage("SetTarget", unitController.target);
+        skeleton.SendMessage("SetTeam", attackable.team);
+    }
+}
---
+++ Assets/Scripts/RaiseDeadEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaiseDeadEffect : MonoBehaviour
+{
+    public float displayTime;
+    public float rotationSpeed;
+
+    private float displayTimer = 0f;
+
+    public void Update()
+    {
+        transform.Rotate(0f, Time.deltaTime * rotationSpeed, 0f);
+
+        displayTimer += Time.deltaTime;
+
+        if (displayTimer >= displayTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
