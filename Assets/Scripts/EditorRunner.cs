1b13730016036735978278b5a58d584d4ac614cd
hli <henrik.lindstrom@lundalogik.se>
2014-11-25 00:56:57 +0100

    Added main menu

---
+++ Assets/Scripts/EditorRunner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorRunner : MonoBehaviour
+{
+    public void Start()
+    {
+        var matchController = GameObject.FindWithTag("MatchController");
+        if (matchController == null)
+            Application.LoadLevel(0);
+    }
+}
