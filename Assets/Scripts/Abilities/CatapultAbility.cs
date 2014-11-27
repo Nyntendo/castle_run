3615cd82697dd511bf1b21eeda0c5d4d7a2b7837
hli <henrik.lindstrom@lundalogik.se>
2014-11-27 14:10:23 +0100

    CATAPULTS ARE AWSOME!!! Also removed some unused colliders on units and buildings

---
+++ Assets/Scripts/Abilities/CatapultAbility.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatapultAbility : MonoBehaviour
+{
+    public GameObject catapultStonePrefab;
+    public Vector3 launchOffset;
+    public float launchForce;
+    public float launchAngle;
+    public float launchDelay;
+
+    public void OnAttack(GameObject target)
+    {
+        StartCoroutine(Launch(target));
+    }
+
+    private IEnumerator Launch(GameObject target)
+    {
+        yield return new WaitForSeconds(launchDelay);
+
+        var stone = Instantiate(
+                catapultStonePrefab,
+                transform.position + launchOffset,
+                Quaternion.identity) as GameObject;
+
+        var launchVector = transform.TransformDirection(Vector3.forward);
+        launchVector = Quaternion.Euler(0, 0, launchAngle) * launchVector;
+        stone.SendMessage("SetSender", gameObject);
+        stone.rigidbody.AddForce(launchVector * launchForce);
+    }
+}
---
+++ Assets/Scripts/Abilities/StoneController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneController : MonoBehaviour
+{
+    public float radius;
+    public int damage;
+    public float activateTime;
+    public float destroyTime;
+    public GameObject impactEffect;
+
+    private bool active = false;
+    private GameObject sender;
+    private Team team;
+
+    public void Start()
+    {
+        collider.isTrigger = true;
+        StartCoroutine(Activate());
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (!active || collision.gameObject == sender)
+            return;
+
+        var colliders = Physics.OverlapSphere(transform.position, radius);
+
+        int i = 0;
+        while (i < colliders.Length)
+        {
+            if (colliders[i].tag == "Unit")
+            {
+                if (colliders[i].gameObject.GetComponent<Attackable>().team != team)
+                    colliders[i].gameObject.SendMessage("Hit", damage);
+            }
+
+            i++;
+        }
+
+        var effect = Instantiate(impactEffect, transform.position, Quaternion.identity) as GameObject;
+        Destroy(effect, destroyTime);
+        active = false;
+        Destroy(gameObject, destroyTime);
+    }
+
+    public void SetSender(GameObject sender)
+    {
+        this.sender = sender;
+        this.team = sender.GetComponent<Attackable>().team;
+    }
+
+    private IEnumerator Activate()
+    {
+        yield return new WaitForSeconds(activateTime);
+        active = true;
+        collider.isTrigger = false;
+    }
+}
